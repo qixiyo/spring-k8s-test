52cd8a4310ce06ec63105f36a7a026041e6f3d6f
qixiyo <1697994271@qq.com>
2023-02-20 12:42:39 +0800

    变更

---
+++ src/main/java/com/test/service/TestService.java
@@ -0,0 +1,7 @@
+package com.test.service;
+
+public class TestService implements test1,test2,test3{
+    public static void main(String[] args) {
+        System.out.println(Math.round(-11.5));
+    }
+}
---
+++ src/main/java/com/test/service/test3.java
@@ -0,0 +1,4 @@
+package com.test.service;
+
+public interface test3 extends test1,test2{
+}
